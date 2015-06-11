86e26dce33e244719bd52b203e24541b4f73aca7
matthewyao <mosmovon@gmail.com>
2015-06-12 00:40:31 +0800

    manage v1.0

---
+++ src/com/tiancai/bean/User.java
@@ -0,0 +1,35 @@
+package com.tiancai.bean;
+
+public class User {
+	private String username;
+	private String nickname;
+	private String email;
+	private int valid;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getValid() {
+		return valid;
+	}
+	public void setValid(int valid) {
+		this.valid = valid;
+	}
+	
+	
+}
