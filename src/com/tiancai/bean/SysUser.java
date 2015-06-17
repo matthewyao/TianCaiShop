3e9e43d5b880489ff3612327179fe3d6a6f05491
matthewyao <mosmovon@gmail.com>
2015-06-18 00:40:59 +0800

    sysuser 5

--- src/com/tiancai/bean/SysUser.java
+++ src/com/tiancai/bean/SysUser.java
@@ -5,4 +5,23 @@ public class SysUser {
 	private String password;
 	private int role;
 	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getRole() {
+		return role;
+	}
+	public void setRole(int role) {
+		this.role = role;
+	}
+	
 }
